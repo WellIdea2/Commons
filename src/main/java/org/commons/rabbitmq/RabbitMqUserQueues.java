5eee44f87efcd9facd89b1e74903ec4bf6677605
Svetlin Dimitrov <svetlin.dimitrov@primeholding.com>
2025-03-01 12:57:06 +0200

    update rabbitMq queues

--- src/main/java/org/commons/rabbitmq/RabbitMqUserQueues.java
+++ src/main/java/org/commons/rabbitmq/RabbitMqUserQueues.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 final public class RabbitMqUserQueues {
 
-  public static final String RECORD_USER_CREATE = "record_user_create_queue";
+  public static final String RECORD_USER_DETAILS_CREATE = "record_user_create_queue";
   public static final String RECORD_USER_DELETION = "record_user_deletion_queue";
+
   public static final String CUSTOM_FOOD_USER_DELETION = "custom_food_user_deletion_queue";
+
+  public static final String USER_DETAILS_USER_DELETION = "user_details_user_deletion_queue";
 
   private RabbitMqUserQueues() {
   }
 
   public static List<String> getAllQueuesUserCreate() {
-    return List.of(RECORD_USER_CREATE);
+    return List.of(RECORD_USER_DETAILS_CREATE);
   }
 
   public static List<String> getAllQueuesUserDeletion() {
-    return List.of(RECORD_USER_DELETION, CUSTOM_FOOD_USER_DELETION);
+    return List.of(RECORD_USER_DELETION, CUSTOM_FOOD_USER_DELETION , USER_DETAILS_USER_DELETION);
   }
 }
